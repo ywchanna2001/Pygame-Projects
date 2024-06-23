b0c0ef6c8bdb0c909522b9bd5b462347e58c8950
Channa Dissanayaka <ywchanna@gmail.com>
2024-06-23 12:39:46 +0530

    added laser and meteor

--- PROJECT/code/main.py
+++ PROJECT/code/main.py
@@ -14,8 +14,17 @@ surf = pygame.Surface((100,200))
 surf.fill('orange')
 x = 100
 
-#importing an image
+#imports
 player_surf = pygame.image.load('images/player.png').convert_alpha()
+player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2 , WINDOW_HEIGHT /2))
+player_dir = 1
+
+meteor_surf = pygame.image.load('images/meteor.png').convert_alpha()
+meteor_rect = meteor_surf.get_frect(center=(WINDOW_WIDTH /2, WINDOW_HEIGHT /2))
+
+laser_surf = pygame.image.load('images/laser.png').convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
+
 star_surf = pygame.image.load('images/star.png').convert_alpha()
 star_positions = [(random.randint(0,WINDOW_WIDTH), random.randint(0,WINDOW_HEIGHT)) for i in range(20)]
 
@@ -33,9 +42,19 @@ while running:
     #stars
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
+    
+    display_surface.blit(meteor_surf,meteor_rect)
+    display_surface.blit(laser_surf,laser_rect)
     #plane
-    x += 0.1
-    display_surface.blit(player_surf, (x, 150 ))
+
+    player_rect.x += player_dir * 0.4
+    if(player_rect.right > WINDOW_WIDTH or player_rect.left < 0):
+        player_dir *= -1 
+    display_surface.blit(player_surf,player_rect )
+    
+
+
+
     pygame.display.update()
 
 
